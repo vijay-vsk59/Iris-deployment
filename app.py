1e90f192d39bc3a541d11ce51158ff5a1826a5b3
vijayvsk7571 <vskvijay59@gmail.com>
2026-04-22 10:26:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import pickle
+import streamlit as st 
+import sklearn
+
+with open("model.pkl", "b") as f:
+    model = pickle.load(f)
+
+# Streamlit UI
+st.title("Iris Flower Classification")
+st.write("Enter the features below : ")
+
+# Input fields
+sl=st.number_input("Sepal Length : ",min_value=4.3,max_value=7.9)
+sw=st.number_input("Sepal Length : ",min_value=2.0,max_value=4.4)
+pl=st.number_input("Sepal Length : ",min_value=1.0,max_value=6.9)
+pw=st.number_input("Sepal Length : ",min_value=0.1,max_value=2.5)
+
+if st.button("Predict"):
+     pr= model.predict([[sl,sw,pl,pw]])
+     classes = ["Setosa", "Versicolor", "Virginica"]
+     st.write(f"Prediction : {classes[pr[0]]}")
